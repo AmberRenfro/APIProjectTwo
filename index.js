ac6bb09fb107f34b25cf672f2c0d2368d87481c8
amber renfro <amber.renfro2021@gmail.com>
2021-08-05 15:05:16 -0500

    final commit

--- index.js
+++ index.js
@@ -20,17 +20,17 @@ function init() {
           console.log(content.data);
           console.log("META", content.meta);
           while (out.firstChild) {
-          out.removeChild(out.firstChild); //1
+          out.removeChild(out.firstChild); 
           }
-          let fig = document.createElement("figure");
+          let output = document.createElement("figure");
           let img = document.createElement("img");
-          let fc = document.createElement("figcaption");
+          let cap = document.createElement("caption");
           img.src = content.data[0].images.downsized.url;
           img.alt = content.data[0].title;
-          fc.textContent = content.data[0].title;
-          fig.appendChild(img);
-          fig.appendChild(fc);
-          out.insertAdjacentElement("afterbegin", fig);
+          cap.textContent = content.data[0].title;
+          output.appendChild(img);
+          output.appendChild(cap);
+          out.insertAdjacentElement("afterbegin", output);
           document.querySelector("#search").value = "";
         })
         .catch(err => {
@@ -46,27 +46,26 @@ document.addEventListener("DOMContentLoaded",initTwo);
 
 function initTwo() {
   document.getElementById("randomButton").addEventListener("click", ev => {
-    ev.preventDefault(); //to stop the page reload
+    ev.preventDefault(); 
     
     let url = `https://api.giphy.com/v1/gifs/random?api_key=${APIKEY}&limit=1&q=`;
     console.log(url);
     fetch(url)
       .then(response => response.json()) 
       .then(content => {
-        //  data, pagination, meta
         console.log(content.data);
         console.log("META", content.meta);
         while (outTwo.firstChild) {
-        outTwo.removeChild(outTwo.firstChild); //1
+        outTwo.removeChild(outTwo.firstChild); 
         }
         let figTwo = document.createElement("figureTwo");
         let imgTwo = document.createElement("img");
-        let fcTwo = document.createElement("figcaptionTwo");
+        let capTwo = document.createElement("captionTwo");
         imgTwo.src = content.data.images.downsized.url;
         imgTwo.alt = content.data.title;
-        fcTwo.textContent = content.data.title;
+        capTwo.textContent = content.data.title;
         figTwo.appendChild(imgTwo);
-        figTwo.appendChild(fcTwo);
+        figTwo.appendChild(capTwo);
         outTwo.insertAdjacentElement("afterbegin", figTwo);
       })
       .catch(err => {
